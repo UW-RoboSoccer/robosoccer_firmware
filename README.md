8faf45d0f2de2457c7e9604e9a8702dedf7da883
rubieluo <rubieluo5@gmail.com>
2025-05-13 19:34:21 -0400

    add subscriber and more publishers

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 Sensor interfacing: Reads IMU, force sensors, encoders
 
 ### Publishes
-* `double`: `/imu/data`: IMU reading from robot
 * `JointState`: `/joint_states`: Robot limb joint states
+* `Float32`: `/imu/data`: IMU reading from robot
+* `Float32`: `/imu/euler_ori`: IMU Euler Orientation from robot
+* `Float32`: `/left/force`: Left foot force reading
+* `Float32`: `/right/force`: Right foot force reading
+
+### Subscribes to
+* `JointState`: `/joint_cmd`: Robot limb joint command
+
--- firmware/src/firmware_bridge_node.cpp
+++ firmware/src/firmware_bridge_node.cpp
@@ -1,46 +1,80 @@
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/Float32.hpp>
+#include <std_msgs/msg/float32.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 #include "firmware_bridge_utils.hpp"
 
 using namespace std::chrono_literals;
 
-class FirmwareBridge : public rclcpp::Node {
+class FirmwareBridgeNode : public rclcpp::Node {
 public:
-  FirmwareBridge()
-  : Node("firmware_bridge")
+  FirmwareBridgeNode()
+  : Node("firmware_bridge_node")
   {
     imu_pub = this->create_publisher<std_msgs::msg::Float32>("/imu/data", 10);
+    imu_euler_pub = this->create_publisher<std_msgs::msg::Float32>("/imu/euler_ori", 10);
+    force_right_pub = this->create_publisher<std_msgs::msg::Float32>("/right/force", 10);
+    force_right_pub = this->create_publisher<std_msgs::msg::Float32>("/left/force", 10);
+    joint_pub = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states", 10);
 
-    joint_pub = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states", 10); // todo need one for each limb?
+    joint_cmd_sub =
+      this->create_subscription<sensor_msgs::msg::JointState>(
+        "/joint_cmd", 10,
+        std::bind(&FirmwareBridgeNode::set_joints_callback, this,
+              std::placeholders::_1));
 
     // Setup Timer to publish at 50Hz (20ms)
     timer_ = this->create_wall_timer(
-      20ms, std::bind(&FirmwareBridge::communication_loop, this));
+      20ms, std::bind(&FirmwareBridgeNode::communication_loop, this));
 
     RCLCPP_INFO(this->get_logger(), "Firmware bridge node has been started.");
   }
 
 private:
+  void set_joints_callback(
+    const sensor_msgs::msg::JointState::SharedPtr msg) {
+      // TODO set joints
+  }
+
   void communication_loop() {
-    auto imu_msg = std_msgs::msg::Float32();
+    auto example_msg = std_msgs::msg::Float32();
 
-    imu_msg.data = 1.0;// todo read imu
-    imu_pub->publish(imu_msg);
+    example_msg.data = 1.0;
+    imu_pub->publish(example_msg);
+    imu_euler_pub->publish(example_msg);
+    force_right_pub->publish(example_msg);
+    force_right_pub->publish(example_msg);
 
-    // todo publish joint states
+    sensor_msgs::msg::JointState joint_state_msg;
+    joint_state_msg.header.stamp = this->get_clock()->now();
+    joint_state_msg.name.clear();
+    joint_state_msg.position.clear();
+    joint_state_msg.velocity.clear();
+    joint_state_msg.effort.clear();
+
+    for (size_t i = 0; i < 16; ++i) { // TODO # of actuators
+      joint_state_msg.position.push_back(0.0);
+      joint_state_msg.velocity.push_back(0.0);
+      joint_state_msg.effort.push_back(0.0);
+    }
+
+    joint_pub->publish(joint_state_msg);
   }
 
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr imu_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr imu_euler_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr force_right_pub;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr force_left_pub;
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_pub;
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr
+      joint_cmd_sub;
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<FirmwareBridge>());
+  rclcpp::spin(std::make_shared<FirmwareBridgeNode>());
   rclcpp::shutdown();
   return 0;
 }
